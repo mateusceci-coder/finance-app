a5c8f6bb14e3c2f55d1c6e74a1a0213d61a52798
Mateus Ceci <mateusceci@gmail.com>
2024-09-27 09:12:38 -0300

    chore: config prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import prettier from 'eslint-config-prettier'
 
 export default [
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-];
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    prettier,
+]
