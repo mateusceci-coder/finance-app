ce8e7763d17c9f579f4615681d78dacafe9a678d
Mateus Ceci <mateusceci@gmail.com>
2024-10-18 09:56:08 -0300

    feat: add PostgresGetUserBalanceRepository

---
+++ src/repositories/postgres/user/get-user-balance.js
@@ -0,0 +1,18 @@
+import { PostgresHelper } from '../../../db/postgres/helper.JS'
+
+export class PostgresGetUserBalanceRepository {
+    async execute(userId) {
+        const balance = await PostgresHelper.query(
+            `
+            SELECT SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS earnings,
+                  SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
+                  SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investments
+            FROM transactions
+            WHERE user_id = $1
+          `,
+            [userId],
+        )
+
+        return balance[0]
+    }
+}
