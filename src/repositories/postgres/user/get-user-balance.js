20f5d2965b2495dd8199881f9345cd79feaf8ca9
Mateus Ceci <mateusceci@gmail.com>
2024-10-23 09:46:44 -0300

    fix: change signals

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -9,8 +9,8 @@ export class PostgresGetUserBalanceRepository {
                   SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investments,
             (
                 SUM (CASE WHEN type = 'EARNING' THEN amount ELSE 0 END)
-                - SUM (CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
-                - SUM (CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)     
+                - (SUM (CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
+                + SUM (CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END))     
             ) AS balance
             FROM transactions
             WHERE user_id = $1
