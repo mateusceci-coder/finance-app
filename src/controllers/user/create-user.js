4416a29b7a7f916abb48ed19d5a6ee2ae4ad5323
Mateus Ceci <mateusceci@gmail.com>
2024-10-24 08:32:31 -0300

    feat: add zod to create user controller

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -1,15 +1,7 @@
 import { EmailAlreadyInUseError } from '../../errors/user.js'
-import {
-    checkIfEmailIsValid,
-    checkIfPasswordIsValid,
-    emailAlreadyInUseResponse,
-    invalidPasswordResponse,
-    badRequest,
-    created,
-    serverError,
-    validateRequiredFields,
-    requiredFieldIsMissingResponse,
-} from '../helpers/index.js'
+import { createUserSchema } from '../../schemas/index.js'
+import { badRequest, created, serverError } from '../helpers/index.js'
+import { z } from 'zod'
 
 export class CreateUserController {
     constructor(createUserUseCase) {
@@ -20,42 +12,16 @@ export class CreateUserController {
         try {
             const params = httpRequest.body
 
-            // validar a requisição (campos obrigatórios e tamanho de senha e e-mail)
-            const requiredFields = [
-                'first_name',
-                'last_name',
-                'email',
-                'password',
-            ]
-
-            const requiredFieldsValidation = validateRequiredFields(
-                params,
-                requiredFields,
-            )
-
-            if (!requiredFieldsValidation.ok) {
-                return requiredFieldIsMissingResponse(
-                    requiredFieldsValidation.missingField,
-                )
-            }
-
-            const passwordIsValid = checkIfPasswordIsValid(params.password)
-
-            if (!passwordIsValid) {
-                return invalidPasswordResponse()
-            }
-
-            const emailIsValid = checkIfEmailIsValid(params.email)
-
-            if (!emailIsValid) {
-                return emailAlreadyInUseResponse()
-            }
+            await createUserSchema.parseAsync(params)
 
             const createdUser = await this.createUserUseCase.execute(params)
 
             // retornar a resposta para o usuário (status code)
             return created(createdUser)
         } catch (error) {
+            if (error instanceof z.ZodError) {
+                return badRequest({ message: error.errors[0].message })
+            }
             if (error instanceof EmailAlreadyInUseError) {
                 return badRequest({ message: error.message })
             }
---
+++ src/schemas/user.js
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+export const createUserSchema = z.object({
+    first_name: z.string().trim().min(3),
+    last_name: z.string().trim().min(3),
+    email: z.string().email().trim().min(3),
+    password: z.string().trim().min(6),
+})
